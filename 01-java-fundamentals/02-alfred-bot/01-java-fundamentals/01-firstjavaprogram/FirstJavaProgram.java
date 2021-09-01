417d564d57cb01a35f627939843ae06970a36a70
Michael Maines <Luckiestpet@gmail.com>
2021-09-01 09:56:44 -0400

    Added changes

--- 01-java-fundamentals/02-alfred-bot/01-java-fundamentals/01-firstjavaprogram/FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("My name is Michael");
-        System.out.println("My age is 20 years in the making!");
-        System.out.println("I was born in Germany");
-    }
-}
