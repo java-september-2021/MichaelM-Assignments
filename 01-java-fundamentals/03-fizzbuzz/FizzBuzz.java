71a7b9623fad3e5850f7295e88e9efaebdd049c1
Michael A Maines <85315191+MMaines2005@users.noreply.github.com>
2021-09-05 02:35:07 -0400

    final

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz{
+    public void count() {
+        for (var i = 1; i <=100; i++){
+            String result = fizzBuzz(i);
+            System.out.printf("Number: %d Result %s \n", i, result);
+        }
+    }
+    public String fizzBuzz(int val){
+        if (val % 3 == 0 && val % 5 == 0){
+            return "FizzBuzz";
+        }else if (val % 3 == 0) {
+            return "Fizz";
+          } else if (val % 5 == 0) {
+            return "Buzz";
+          } else {
+            return Integer.toString(val);
+    }
+    }
+}
---
+++ 01-java-fundamentals/03-fizzbuzz/FizzbuzzTester.java
@@ -0,0 +1,7 @@
+public class FizzbuzzTester{
+
+    public static void main(String[] args){
+        FizzBuzz tester = new FizzBuzz();
+        tester.count();
+    }
+}
