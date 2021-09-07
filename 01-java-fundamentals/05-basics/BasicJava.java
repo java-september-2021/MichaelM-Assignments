1cea252019033a61feb810f0499c3def8e62a758
Michael A Maines <85315191+MMaines2005@users.noreply.github.com>
2021-09-07 04:47:15 -0400

    First Save

---
+++ 01-java-fundamentals/05-basics/BasicJava.java
@@ -0,0 +1,68 @@
+public class BasicJava{
+    // Print 1-255
+    public static void print255(){
+        for (var i = 0; i <= 255; i++);
+        return i;
+    }
+
+    // Print odd numbers between 1-255
+    public static void printOdd255(){
+
+    }
+
+    // Sigma of 255 AKA Sum to 255
+    public static void printSum255(){
+        
+    
+    }
+
+    // Iterating through an array
+    public static void arrayIteration(){
+
+    }
+
+    // Find Max
+    public static void findMax(){
+
+    }
+
+    // Array with Odd Numbers
+    public static void arrayOdd(){
+
+    }
+
+    // Ninja Bonuses (Optional):
+
+    // Get Average
+    public static void getAverage(){
+
+    }
+
+    // Greater Than Y
+    public static void greaterY(){
+
+    }
+
+    // Square the values
+    public static void squareVal(){
+
+    }
+
+    // Eliminate Negative Numbers
+    public static void deleteNeg(){
+
+    }
+
+    // Sensei Bonuses (Optional):
+
+    // Max, Min, and Average
+    public static void minMaxAverage(){
+
+    }
+
+    // Shifting the Values in the Array
+    public static void shiftingArray(){
+
+    }
+
+}
---
+++ 01-java-fundamentals/05-basics/BasicJavaTest.java
@@ -0,0 +1,6 @@
+public class BasicJavaTest{
+    public static void main(String[] args) {
+        print255 k = new print255();
+        System.out.println(k);
+    }
+}
